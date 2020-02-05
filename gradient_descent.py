63964915b8204ca2d916e455535b66baff454b0d
Josh <heyjoshkruse@gmail.com>
2020-02-05 16:01:32 -0700

    tried to create the prediction vector but doesn't look like it is creating a valid prediction

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sklearn.metrics
 from sklearn.preprocessing import scale
 
 # function: calc_gradient
@@ -35,7 +36,7 @@ def gradient_descent( X, y, stepSize, maxIterations) :
     weightMatrix = np.array(np.zeros(num_of_entries).reshape(features, maxIterations))
 
 
-    for index in range(1, maxIterations) :
+    for index in range(maxIterations) :
         # first compute the gradient given the current weightVector
         #   make sure that the gradient is of the mean logistic loss over all training data
         gradient = calc_gradient(weightVector, y, X)
@@ -49,26 +50,27 @@ def gradient_descent( X, y, stepSize, maxIterations) :
 
     return weightMatrix
 
-#X = np.array([[1, 2, 3], [3, 4, 5]])
-#y = np.array([0,1])
-#print(gradient_descent(X, y, 0.1, 50))
+# read data from csv
+all_data = np.genfromtxt('spam.data', delimiter=" ")
+# get size of data
+data_size = all_data.shape[1] - 1
 
-def read_csv() :
-    # read data from csv
-    all_data = np.genfromtxt('spam.data', delimiter=" ")
-    # get size of data
-    data_size = all_data.shape[1] - 1
-    # set inputs to everything but last col, and scale
-    inputs = np.delete(all_data, data_size, axis=1)
-    inputs_scaled = scale(inputs)
-    # set outputs to last col of data
-    outputs = all_data[:,data_size]
+# set inputs to everything but last col, and scale
+inputs = np.delete(all_data, data_size, axis=1)
+inputs_scaled = scale(inputs)
+# set outputs to last col of data
+outputs = all_data[:,data_size]
 
-    #print(all_data)
-    #print(inputs)
-    #print(inputs_scaled)
-    #print(outputs)
+#print(all_data)
+#print(inputs)
+#print(inputs_scaled)
+#print(outputs)
 
-    print(gradient_descent(inputs_scaled, outputs, 0.1, 100))
+weightMatrix = gradient_descent(inputs_scaled, outputs, 0.1, 100)
 
-read_csv()
+print(inputs_scaled)
+
+predicted = np.dot(inputs, weightMatrix)
+print(predicted)
+
+print(np.mean( outputs != predicted[:,0]))
