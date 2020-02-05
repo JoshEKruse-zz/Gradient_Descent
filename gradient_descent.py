18bb0cd7ad27364dfeeefd91fe4722db5c2ee869
Josh <heyjoshkruse@gmail.com>
2020-02-05 14:55:37 -0700

    created working gradient descent function

---
+++ gradient_descent.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# function: calc_gradient
+# calculates the gradient for a specific weightVector
+# returns: the mean of the gradients as a vector
+def calc_gradient(weightVector, y, X) :
+    sum = 0
+    size = y.shape[0]
+    for index in range(size) :
+        y_tild = 0
+        if(y[index] == 0) : y_tild = -1
+        if(y[index] == 1) : y_tild = 1
+        #print(X[index])
+        #print("top" + str(-y_tild * X[index]))
+        #print((1 + np.exp(y_tild * weightVector * X[index])))
+        gradient = ((-y_tild * X[index]) / (1 + np.exp(y_tild * weightVector * X[index])))
+        #print("grad" + str(gradient))
+        sum += gradient
+    return sum / size
+
+# function: gradient_descent
+# calculates the gradient descent for a given X matrix with corresponding y vector
+def gradient_descent( X, y, stepSize, maxIterations) :
+
+    # declare weightVector which is initialized to the zero vector
+    #   one element for each feature
+    dimension = X.shape
+    features = dimension[1]
+    weightVector = np.zeros(features)
+
+    # declare weightMatrix of real number
+    #   number of rows = features, number of cols = maxIterations
+    num_of_entries = features * maxIterations
+    weightMatrix = np.array(np.zeros(num_of_entries).reshape(features, maxIterations))
+
+
+    for index in range(1, maxIterations) :
+        # first compute the gradient given the current weightVector
+        #   make sure that the gradient is of the mean logistic loss over all training data
+        gradient = calc_gradient(weightVector, y, X)
+
+        # then update weightVector by taking a step in the negative gradient direction
+        weightVector = weightVector - stepSize * gradient
+
+        # then store the resulting weightVector in the corresponding column of weightMatrix
+        for row in range(features) :
+            weightMatrix[row][index] = weightVector[row]
+
+    return weightMatrix
+
+#X = np.array([[1, 2, 3], [3, 4, 5]])
+#y = np.array([0,1])
+#print(gradient_descent(X, y, 0.1, 50))
