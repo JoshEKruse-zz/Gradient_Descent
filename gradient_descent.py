8151048bca75a64c9b40d5ba342b5be25ab7bb02
Josh <heyjoshkruse@gmail.com>
2020-02-06 14:54:14 -0700

    completed log reg

--- gradient_descent.py
+++ gradient_descent.py
@@ -57,6 +57,10 @@ def gradient_descent( X, y, stepSize, maxIterations) :
 
     return weightMatrix
 
+def sigmoid(x) :
+    x = 1 / (1 + np.exp(-x))
+    return x
+
 # read data from csv
 all_data = np.genfromtxt('spam.data', delimiter=" ")
 # get size of data
@@ -81,17 +85,24 @@ print("{: >11} {: >4} {: >4}".format("validation", np.sum(y_val==0), np.sum(y_va
 maxIterations = 500
 weightMatrix = gradient_descent(X_train, y_train, 0.2, maxIterations)
 
+sig_v = np.vectorize(sigmoid)
+
 # calculate predicted matrix, round each answer
-train_pred = np.around(np.dot( X_train, weightMatrix))
+train_pred = np.dot( X_train, weightMatrix)
 val_pred = np.around(np.dot( X_val, weightMatrix))
+test_pred = np.around(np.dot( X_test, weightMatrix))
+
+train_pred = np.around(sig_v(train_pred))
+val_pred = np.around(sig_v(val_pred))
+test_pred = np.around(sig_v(test_pred))
 
 # calculate percent error
 train_result = []
 val_result = []
 for index in range(maxIterations) :
-    train_result.append( np.mean( y_train == train_pred[:,index]))
+    train_result.append( np.mean( y_train != train_pred[:,index]))
 for index in range(maxIterations):
-    val_result.append(np.mean( y_val == val_pred[:, index]))
+    val_result.append(np.mean( y_val != val_pred[:, index]))
 train_min_index, train_min_value = min(enumerate(train_result), key=operator.itemgetter(1))
 val_min_index, val_min_value = min(enumerate(val_result), key=operator.itemgetter(1))
 
@@ -130,6 +141,7 @@ ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_mi
 line2, = ax.plot(val_result, "r-", label='validation')
 ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value+.1),
             arrowprops=dict(facecolor='red', shrink=0.05),)
+
 ax.set_ylabel("Loss")
 ax.set_xlabel("Iterations")
 ax.set_title("Logistic Loss")
@@ -137,8 +149,31 @@ ax.set_ylim(0,.8)
 ax.legend()
 pyplot.show()
 
+best_tr_pred = train_pred[:,val_min_index]
+best_va_pred = val_pred[:,val_min_index]
+best_te_pred = test_pred[:,val_min_index]
+
+zero_vector = np.zeros(100)
+
+print(np.mean(zero_vector == zero_vector))
+
+train_error = int(np.mean(y_train != best_tr_pred) * 100)
+val_error = int(np.mean(y_val != best_va_pred) * 100)
+test_error = int(np.mean(y_test != best_te_pred) * 100)
+
+baseline_train = np.empty(np.sum(y_train == 0))
+
+for index in range(y_train.shape[0]) :
+    if(y_train[index] == 0) :
+        np.append(baseline_train, best_tr_pred[index])
+
+y_baseline = np.zeros(baseline_train.shape[0])
+
+base_train_error = np.mean(y_baseline != baseline_train)
+print(base_train_error)
+
 # create log reg table of errors
 print("{: >11} {: >9} {: >9}".format("", "log reg", "baseline"))
-print("{: >11} {: >9} {: >9}".format("train","",""))
-print("{: >11} {: >9} {: >9}".format("validation","",""))
-print("{: >11} {: >9} {: >9}".format("test", np.mean(log_reg_results),""))
+print("{: >11} {: >9} {: >9}".format("train", str(train_error) + "%", str(base_train_error) + "%"))
+print("{: >11} {: >9} {: >9}".format("validation", str(val_error) + "%",""))
+print("{: >11} {: >9} {: >9}".format("test", str(test_error) + "%", ""))
