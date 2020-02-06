completed all graphs and fixed problems with gradient formula. Next steps are to calculate ROC curve and table of error

gradient_descent.py
@@ -1,5 +1,4 @@
 import operator
-
 import numpy as np
 import sklearn.metrics
 from matplotlib import pyplot
@@ -9,12 +8,8 @@ from sklearn.model_selection import train_test_split
 # function: calc_gradient
 # calculates the gradient for a specific weightVector
 # returns: the mean of the gradients as a vector
-def calc_gradient(weightVector, y, X) :
-    size = y.shape[0]
-    y_tild = np.empty(size)
-    for index in range(size) :
-        if(y[index] == 0) : y_tild[index] = -1
-        if(y[index] == 1) : y_tild[index] = 1
+def calc_gradient(weightVector, y_tild, X) :
+    size = y_tild.shape[0]
 
     sum = 0
     for index in range(size) :
@@ -41,12 +36,17 @@ def gradient_descent( X, y, stepSize, maxIterations) :
     num_of_entries = features * maxIterations
     weightMatrix = np.array(np.zeros(num_of_entries).reshape(features, maxIterations))
 
+    size = y.shape[0]
+    y_tild = np.empty(size)
+    for index in range(size):
+        if (y[index] == 0): y_tild[index] = -1
+        else : y_tild[index] = 1
 
     for index in range(maxIterations) :
         # first compute the gradient given the current weightVector
         #   make sure that the gradient is of the mean logistic loss over all training data
         #print(weightVector)
-        gradient = calc_gradient(weightVector, y, X)
+        gradient = calc_gradient(weightVector, y_tild, X)
 
         # then update weightVector by taking a step in the negative gradient direction
         weightVector = weightVector - stepSize * gradient
@@ -56,13 +56,6 @@ def gradient_descent( X, y, stepSize, maxIterations) :
             weightMatrix[row][index] = weightVector[row]
 
     return weightMatrix
-
-def matprint(mat, fmt="g"):
-    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
-    for x in mat:
-        for i, y in enumerate(x):
-            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
-        print("")
 
 # read data from csv
 all_data = np.genfromtxt('spam.data', delimiter=" ")
@@ -78,15 +71,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
 # print sizes of each set
-print("           " + "y    " + "    ")
-print("set        " + str(0) + "    " + str(1))
-print("train      " + str(np.sum(y_train == 0)) + " " + str(np.sum(y_train == 1)))
-print("test       " + str(np.sum(y_test == 0)) + "  " + str(np.sum(y_test == 1)))
-print("validation " + str(np.sum(y_val == 0)) + "  " + str(np.sum(y_val == 1)))
+print("{: >11} {: >4} {: >4}".format("", "y", ""))
+print("{: >11} {: >4} {: >4}".format("set", 0, 1))
+print("{: >11} {: >4} {: >4}".format("train", np.sum(y_train==0), np.sum(y_train==1)))
+print("{: >11} {: >4} {: >4}".format("test", np.sum(y_test==0), np.sum(y_test==1)))
+print("{: >11} {: >4} {: >4}".format("validation", np.sum(y_val==0), np.sum(y_val==1)))
 
 # get weightMatrix
-maxIterations = 1000
-weightMatrix = gradient_descent(X_train, y_train, 0.4, maxIterations)
+maxIterations = 500
+weightMatrix = gradient_descent(X_train, y_train, 0.2, maxIterations)
 
 # calculate predicted matrix, round each answer
 train_pred = np.around(np.dot( X_train, weightMatrix))
@@ -104,12 +97,12 @@ val_min_index, val_min_value = min(enumerate(val_result), key=operator.itemgette
 
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
-line1, = ax.plot(train_result, label='train')
-ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value),
-            arrowprops=dict(facecolor='black', shrink=0.05),)
-line2, = ax.plot(val_result, label='validation')
-ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value+.01),
-            arrowprops=dict(facecolor='black', shrink=0.05),)
+line1, = ax.plot(train_result, "g-", label='train')
+ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value-.1),
+            arrowprops=dict(facecolor='green', shrink=0.05),)
+line2, = ax.plot(val_result, "r-", label='validation')
+ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value+.1),
+            arrowprops=dict(facecolor='red', shrink=0.05),)
 ax.set_ylabel("Percent")
 ax.set_xlabel("Iterations")
 ax.set_title("Percent Error")
@@ -131,15 +124,21 @@ val_min_index, val_min_value = min(enumerate(val_result), key=operator.itemgette
 
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
-line1, = ax.plot(train_result, label='train')
-ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value),
-            arrowprops=dict(facecolor='black', shrink=0.05),)
-line2, = ax.plot(val_result, label='validation')
-ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value+.01),
-            arrowprops=dict(facecolor='black', shrink=0.05),)
+line1, = ax.plot(train_result, "g-", label='train')
+ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value-.1),
+            arrowprops=dict(facecolor='green', shrink=0.05),)
+line2, = ax.plot(val_result, "r-", label='validation')
+ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value+.1),
+            arrowprops=dict(facecolor='red', shrink=0.05),)
 ax.set_ylabel("Loss")
 ax.set_xlabel("Iterations")
 ax.set_title("Logistic Loss")
 ax.set_ylim(0,.8)
 ax.legend()
 pyplot.show()
+
+# create log reg table of errors
+print("{: >11} {: >9} {: >9}".format("", "log reg", "baseline"))
+print("{: >11} {: >9} {: >9}".format("train","",""))
+print("{: >11} {: >9} {: >9}".format("validation","",""))
+print("{: >11} {: >9} {: >9}".format("test", np.mean(log_reg_results),""))