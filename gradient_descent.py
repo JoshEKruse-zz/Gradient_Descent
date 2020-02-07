created a ROC graph of the logistic regression and baseline. I think something is wrong with our gradient calculation but i honestly dont know how to fix

gradient_descent.py
@@ -4,6 +4,7 @@ import sklearn.metrics
 from matplotlib import pyplot
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve
 
 # function: calc_gradient
 # calculates the gradient for a specific weightVector
@@ -83,8 +84,8 @@ print("{: >11} {: >4} {: >4}".format("test", np.sum(y_test==0), np.sum(y_test==1
 print("{: >11} {: >4} {: >4}".format("validation", np.sum(y_val==0), np.sum(y_val==1)))
 
 # get weightMatrix
-maxIterations = 400
-weightMatrix = gradient_descent(X_train, y_train, 0.1, maxIterations)
+maxIterations = 1000
+weightMatrix = gradient_descent(X_train, y_train, 0.05, maxIterations)
 
 # vectorize sigmoid function to pass on prediction matrix
 sig_v = np.vectorize(sigmoid)
@@ -115,10 +116,10 @@ print(val_result)
 # graph percent error
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
-line1, = ax.plot(train_result, "g-", label='train')
+line1, = ax.plot(train_result, "g-", label='Train')
 ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value),
             arrowprops=dict(facecolor='green', shrink=0.05),)
-line2, = ax.plot(val_result, "r-", label='validation')
+line2, = ax.plot(val_result, "r-", label='Validation')
 ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value),
             arrowprops=dict(facecolor='red', shrink=0.05),)
 ax.set_ylabel("Percent")
@@ -138,13 +139,16 @@ for index in range(maxIterations):
 train_min_index, train_min_value = min(enumerate(train_loss_results), key=operator.itemgetter(1))
 val_min_index, val_min_value = min(enumerate(val_loss_results), key=operator.itemgetter(1))
 
+print(train_loss_results)
+print(val_loss_results)
+
 # plot logistic loss
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
-line1, = ax.plot(train_loss_results, "g-", label='train')
+line1, = ax.plot(train_loss_results, "g-", label='Train')
 ax.annotate('train min', xy=(train_min_index, train_min_value), xytext=(train_min_index, train_min_value),
             arrowprops=dict(facecolor='green', shrink=0.05),)
-line2, = ax.plot(val_loss_results, "r-", label='validation')
+line2, = ax.plot(val_loss_results, "r-", label='Validation')
 ax.annotate('validation min', xy=(val_min_index, val_min_value), xytext=(val_min_index, val_min_value),
             arrowprops=dict(facecolor='red', shrink=0.05),)
 
@@ -165,34 +169,33 @@ train_error = int(np.mean(y_train != best_tr_pred) * 100)
 val_error = int(np.mean(y_val != best_va_pred) * 100)
 test_error = int(np.mean(y_test != best_te_pred) * 100)
 
-# initalize an empty vector for baseline
-baseline_train = []
-baseline_val = []
-baseline_test = []
-
-# add baseline values to vector
-for index in range(y_train.shape[0]) :
-    if(y_train[index] == 0) :
-        baseline_train.append(best_tr_pred[index])
-for index in range(y_val.shape[0]) :
-    if(y_val[index] == 0) :
-        baseline_val.append(best_va_pred[index])
-for index in range(y_test.shape[0]) :
-    if(y_test[index] == 0) :
-        baseline_test.append(best_te_pred[index])
-
 # create zero vector for y baseline
-y_train_baseline = np.zeros(len(baseline_train))
-y_val_baseline = np.zeros(len(baseline_val))
-y_test_baseline = np.zeros(len(baseline_test))
+y_train_baseline = np.zeros(best_tr_pred.shape[0])
+y_val_baseline = np.zeros(best_va_pred.shape[0])
+y_test_baseline = np.zeros(best_te_pred.shape[0])
 
-# calculate baseline error
-base_train_error = int(np.mean(y_train_baseline != baseline_train)*100)
-base_val_error = int(np.mean(y_val_baseline != baseline_val)*100)
-base_test_error = int(np.mean(y_test_baseline != baseline_test)*100)
+# calculate logistic regression train error
+baseline_train_error = int(np.mean(y_train != y_train_baseline) * 100)
+baseline_val_error = int(np.mean(y_val != y_val_baseline) * 100)
+baseline_test_error = int(np.mean(y_test != y_test_baseline) * 100)
 
 # create log reg table of errors, baseline errors
 print("{: >11} {: >9} {: >9}".format("", "log reg", "baseline"))
-print("{: >11} {: >9} {: >9}".format("train", str(train_error) + "%", str(base_train_error) + "%"))
-print("{: >11} {: >9} {: >9}".format("validation", str(val_error) + "%", str(base_val_error) + "%"))
-print("{: >11} {: >9} {: >9}".format("test", str(test_error) + "%", str(base_test_error) + "%"))
+print("{: >11} {: >9} {: >9}".format("train", str(train_error) + "%", str(baseline_train_error) + "%"))
+print("{: >11} {: >9} {: >9}".format("validation", str(val_error) + "%", str(baseline_val_error) + "%"))
+print("{: >11} {: >9} {: >9}".format("test", str(test_error) + "%", str(baseline_test_error) + "%"))
+
+# calculate roc curves for logistic regression and baseline
+fpr_log, tpr_log, _ = roc_curve(y_test, sig_v(np.dot(X_test, weightMatrix))[:,val_min_index])
+fpr_base, tpr_base, _ = roc_curve(y_test, y_test_baseline, pos_label=1)
+
+pyplot.figure()
+pyplot.plot(fpr_log, tpr_log, "b-", label="Logistic Regression") # plot logistic regression
+pyplot.plot(fpr_base, tpr_base, "m-", label="Baseline") # plot baseline
+pyplot.xlabel('False Positive Rate')
+pyplot.ylabel('True Positive Rate')
+pyplot.xlim([0.0, 1.0])
+pyplot.ylim([0.0, 1.05])
+pyplot.title('Receiver Operating Characteristic')
+pyplot.legend(loc="lower right")
+pyplot.show()